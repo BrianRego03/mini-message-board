2ce358f2d1288b3da02c2a436e0b8b65a99fcca6
brianrego03 <brian.rego03@gmail.com>
2025-05-06 16:32:01 +0530

    Connect to hosted db

--- db/pool.js
+++ db/pool.js
@@ -2,4 +2,8 @@ require('dotenv').config();
 const {Pool}=require("pg");
 
 
-module.exports=new Pool({connectionString:process.env.DATABASE_URL,})
+module.exports=new Pool({connectionString:process.env.DATABASE_URL,
+    ssl:{
+        rejectUnauthorized: false
+    }
+})
